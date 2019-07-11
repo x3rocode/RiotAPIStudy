bcb708ca790ce7bfaf3eba42243130539bc98b48
dall4e <dall4e@users.noreply.github.com>
2019-07-11 17:16:10 +0900

    example added.

---
+++ Example/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HamsterRiotAPI;
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HamsterRiotAPI.Config.APIKey = "YOUR_API_KEY";
+            var obj = HamsterRiot.GetSummonerByName("YOUR_NAME");
+            Console.WriteLine(obj.name);
+        }
+    }
+}
