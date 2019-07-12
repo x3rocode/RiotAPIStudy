f4e9bca1f6e8f89d6769692d96fc0327ccaf7b7e
dall4e <dall4e@users.noreply.github.com>
2019-07-12 09:46:19 +0900

    project added

--- LoLAPITest/RiotAPI/HamstetRiot.cs
+++ LoLAPITest/RiotAPI/HamstetRiot.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HamsterRiotAPI.Structure;
+using HamsterRiotAPI.HttpUtils;
+
 namespace HamsterRiotAPI
 {
     /// <summary>
---
+++ WatchYourBoyFriend/Program.cs
@@ -0,0 +1,23 @@
+﻿using HamsterRiotAPI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WatchYourBoyFriend
+{
+    class Program
+    {
+        static void ReadAPIKey()
+        {
+            var p = File.ReadAllLines("apikey/apikey.txt");
+
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
